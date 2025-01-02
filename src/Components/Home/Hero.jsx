2afc3d3412ef0e9ed98ce52d697b454779d32a9d
mysterecode <134565637+mysterecode@users.noreply.github.com>
2025-01-02 15:12:11 +0100

    Update Hero.jsx

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../../assets/css/hero.css';
 
 function Hero() {
@@ -7,8 +8,8 @@ function Hero() {
                 <h1>Style Redefined</h1>
                 <p>Explore our premium collection of men’s clothing. Elevate your wardrobe today.</p>
                 <div className="Hero-buttons">
-                    <button className="btn btn-primary">Shop Now</button>
-                    <button className="btn btn-secondary">Learn More</button>
+                    <Link to="/Casual-Wear"><button className="btn btn-primary">Shop Now</button></Link>
+                    <Link to="/Casual-wear"><button className="btn btn-secondary">Learn More</button></Link>
                 </div>
             </div>
         </div>
